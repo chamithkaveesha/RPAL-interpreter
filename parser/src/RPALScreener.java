c31d3d5a2b0fd88f5e0fdaf3fc8cf595252ed3ea
Ke-vin-S <kevinxsanjula@gmail.com>
2025-03-31 15:27:12 +0530

    Screener

---
+++ parser/src/RPALScreener.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RPALScreener implements Screener {
+    List<Token> out = new ArrayList<>();
+    @Override
+    public List<Token> screen(List<Token> tokens) {
+        for (Token token : tokens) {
+            if (token.getType() != TokenType.DELETE) {
+                out.add(token);
+            }
+        }
+        if (tokens.get(tokens.size() - 1).getType() == TokenType.DELETE) {
+            Token last = tokens.get(tokens.size() - 1);
+            out.add(new Token(TokenType.EOF, "EOF", last.getLine(), last.getColumn()));
+        }
+        return out;
+    }
+}
