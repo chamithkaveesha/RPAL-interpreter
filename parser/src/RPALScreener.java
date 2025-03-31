faf5f6af6dc8f70b5fb2ef45df32e2075bc4e599
Ke-vin-S <kevinxsanjula@gmail.com>
2025-03-31 15:33:33 +0530

    refactor Token.java

--- parser/src/RPALScreener.java
+++ parser/src/RPALScreener.java
@@ -6,13 +6,13 @@ public class RPALScreener implements Screener {
     @Override
     public List<Token> screen(List<Token> tokens) {
         for (Token token : tokens) {
-            if (token.getType() != TokenType.DELETE) {
+            if (token.type() != TokenType.DELETE) {
                 out.add(token);
             }
         }
-        if (tokens.get(tokens.size() - 1).getType() == TokenType.DELETE) {
+        if (tokens.get(tokens.size() - 1).type() == TokenType.DELETE) {
             Token last = tokens.get(tokens.size() - 1);
-            out.add(new Token(TokenType.EOF, "EOF", last.getLine(), last.getColumn()));
+            out.add(new Token(TokenType.EOF, "EOF", last.line(), last.column()));
         }
         return out;
     }
--- parser/src/Token.java
+++ parser/src/Token.java
@@ -1,31 +1,4 @@
-public class Token {
-    private TokenType type;
-    private final String lexeme;
-    private final int line;
-    private final int column;
-
-    public Token(TokenType type, String lexeme, int line, int column) {
-        this.type = type;
-        this.lexeme = lexeme;
-        this.line = line;
-        this.column = column;
-    }
-
-    public TokenType getType() {
-        return type;
-    }
-    public void setType(TokenType type) {
-        this.type = type;
-    }
-    public int getLine() {
-        return line;
-    }
-    public int getColumn() {
-        return column;
-    }
-    public String getLexeme() {
-        return lexeme;
-    }
+public record Token(TokenType type, String lexeme, int line, int column) {
 
     @Override
     public String toString() {
